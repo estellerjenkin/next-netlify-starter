e48602e7ede7bdfe1a4f9e1cac5700c782600167
estellerjenkin <110261742+estellerjenkin@users.noreply.github.com>
2022-07-30 03:14:56 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,36 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+ProDentim™
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main>
-        <Header title="WelAAome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+United States
 
-      <Footer />
-    </div>
-  )
-}
+Choose Your Country
+
+Get 96% Off Today!  Hurry UP! Limited Time Only
+
+
+
+
+
+
+Other Countries
+
+ 
+
+This site is not a part of the Facebook website or Facebook Inc. Additionally, this site is not endorsed by Facebook in any way. FACEBOOK is a trademark of FACEBOOK, Inc.
+
+Affiliate Disclaimer: Keep in mind that we may receive commissions when you click our links and make purchases. However, this does not impact our reviews and comparisons. We try our best to keep things fair and balanced, in order to help you make the best choice for you.
+
+privacy-policy
+
+Terms and Conditions
+
+2022 All Rights Reserved By Antreya
+
+This is a custom code placeholder.
+Switch to Preview or publish the page
+to see how your code works. Double-click to edit
+
+<p align="center"><a href="/index.html"><FONT COLOR="#FFFFFF">Home</FONT></a> | <a href="/index.html"><FONT COLOR="#FFFFFF">Top Vote</FONT></a> | <a href="/index.html"><FONT COLOR="#FFFFFF">Reviews</FONT></a> | <a href="/index.html"><FONT COLOR="#FFFFFF">FAQ's</FONT></a> | <a href="/index.html"><FONT COLOR="#FFFFFF">Contact Us</FONT></a> | <a href="/index.html"><FONT COLOR="#FFFFFF">Articles</FONT></a> | <a href="/index.html"><FONT COLOR="#FFFFFF">Disclaimer</FONT></a> | <a href="/index.html"><FONT COLOR="#FFFFFF">Privacy</FONT></a><br/> <FONT COLOR="#FFFFFF">© 2021. All Rights Reserved.<br />
+
+ 
+
